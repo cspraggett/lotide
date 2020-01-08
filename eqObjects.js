e7f6ec69307a0a7d9591046ff74a72692b5cda1d
cspraggett <cspraggett@gmail.com>
2020-01-08 18:18:33 +0000

    Added functionality to check arrays in objects for equality

--- eqObjects.js
+++ eqObjects.js
@@ -3,10 +3,23 @@ const assertEqual = (actual, expected) =>
     ? console.log(`✅ ✅ ✅ consoleAssertion Passed: ${actual} === ${expected}`)
     : console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`);
 
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
 const eqObjects = (ob1, ob2) => {
   if (Object.keys(ob1).length !== Object.keys(ob2).length) return false;
   for (const keys of Object.keys(ob1)) {
-    if (ob1[keys] !== ob2[keys]) return false;
+    if (Array.isArray(ob1[keys])) {
+      if (!Array.isArray(ob2[keys])) return false;
+      if (!(eqArrays(ob1[keys], ob2[keys]))) {
+        return false;
+      }
+    } else if (ob1[keys] !== ob2[keys]) return false;
   }
   return true;
 };
@@ -17,3 +30,11 @@ assertEqual(eqObjects(ab, ba), true);
 
 const abc = { a: '1', b: '2', c: '3'};
 assertEqual(eqObjects(ab, abc), false); // => false
+
+
+const cd = { c: "1", d: ["2", 3]};
+const dc = { d: ["2", 3] , c: "1"};
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2,", 3, 4]};
+assertEqual(eqObjects(cd, cd2), false);
