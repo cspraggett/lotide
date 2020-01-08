e86160f682ac6a9818a0afad0ba89e7e13972cc8
cspraggett <cspraggett@gmail.com>
2020-01-08 14:57:05 +0000

    added assertion tests to letterPositions.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,16 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  eqArrays(arr1, arr2) ? console.log(`✅ ✅ ✅ ${arr1} === ${arr2}`) :
+    console.log(`🛑 🛑 🛑 ${arr1} !== ${arr2}`);
+};
+
 const letterPositions = sentence => {
   const results = {};
   sentence.toLowerCase().replace(/\s/g, '').split('').map((value, i) =>
@@ -6,4 +19,8 @@ const letterPositions = sentence => {
   return results;
 };
 
-console.log(letterPositions('Hello'));
+assertArraysEqual(letterPositions('hello').e, [1]);
+assertArraysEqual(letterPositions('hello').h, [0]);
+assertArraysEqual(letterPositions('hello').l, [2,3]);
+assertArraysEqual(letterPositions('hello').o, [4]);
+assertArraysEqual(letterPositions('lighthouse in the house').h, [3,5,13,15]);
