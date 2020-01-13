af23a1501e2fea23be50247cc4da9c5f1761734f
cspraggett <cspraggett@gmail.com>
2020-01-13 19:42:12 +0000

    created index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const findKeyByValue = require("./findKeyByValue");
+const findKey = require("./findKey");
+const flatten = require("./flatten");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  countLetters,
+  countOnly,
+  findKeyByValue,
+  findKey,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without
+};
