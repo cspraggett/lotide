6f828e5a6b45bfbc6c96bef300c53ce3fc18c9a1
Christopher Spraggett <cspraggett@gmail.com>
2020-01-13 12:28:13 -0500

    updated eqArrays and created eqArraysTest.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,7 +1,4 @@
-const assertEqual = (actual, expected) =>
-  actual === expected
-    ? console.log(`✅ ✅ ✅ consoleAssertion Passed: ${actual} === ${expected}`)
-    : console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`);
+const assertEqual = require("./assertEqual");
 
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) return false;
@@ -15,21 +12,4 @@ const eqArrays = (arr1, arr2) => {
   return true;
 };
 
-// assertEqual(eqArrays([1,2,3], [1,2,3]), true);
-// assertEqual(eqArrays([1,2,3],[1,2,'3']),false);
-// assertEqual(eqArrays(['a','b','c'],['A','B','C']),false);
-// assertEqual(eqArrays(['x'],['x']),true);
-
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-
-assertEqual(
-  eqArrays(
-    [[2, 3], [4]],
-    [
-      [2, 3],
-      [4, 5]
-    ]
-  ),
-  false
-); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
+module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,21 @@
+const eqArrays = require("../eqArrays");
+const assertEqual = require("../assertEqual");
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false);
+assertEqual(eqArrays(["a", "b", "c"], ["A", "B", "C"]), false);
+assertEqual(eqArrays(["x"], ["x"]), true);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+
+assertEqual(
+  eqArrays(
+    [[2, 3], [4]],
+    [
+      [2, 3],
+      [4, 5]
+    ]
+  ),
+  false
+); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
